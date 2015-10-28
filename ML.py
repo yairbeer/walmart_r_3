e05729b7df8c9f5c7bf08454aaab668de9dd733b
Yair Beer <yaia1309@gmail.com>
2015-10-28 13:02:53 +0200

    moved to py file

---
+++ ML.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import random
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import log_loss
+from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import KFold
+
+__author__ = 'WiBeer'
+
+"""
+data ML
+"""
+train = pd.read_csv("train_dummied.csv")
+train_result = pd.read_csv("train_result.csv")
+test = pd.read_csv("test_dummied.csv")
+
+# Common preprocessing
+# Standardizing
+stding = StandardScaler()
+train = stding.fit_transform(train)
+test = stding.transform(test)
+
+classifier = RandomForestClassifier(n_estimators=300)
+
+# # CV n = 4
+# cv_n = 4
+
+# metric = []
+# for train_index, test_index in kf:
+#     X_train, X_test = train_cv[train_index, :], train_cv[test_index, :]
+#     y_train, y_test = train_result_cv[train_index].ravel(), train_result_cv[test_index].ravel()
+#     print X_train.shape, X_test.shape
+#     print len(y_train), len(y_test )
+#     # train machine learning
+#     classifier.fit(X_train, y_train)
+
+#     # predict
+#     class_pred = classifier.predict_proba(X_test)
+
+#     # evaluate
+#     print log_loss(y_test, class_pred)
+#     metric.append(log_loss(y_test, class_pred))
+# print 'The log loss is: ', np.mean(metric)
+
+# predict testset
+classifier.fit(train, train_result)
+predicted_results = classifier.predict_proba(test)
+
+predicted_results_self = classifier.predict_proba(train)
+log_loss(train_result, predicted_results_self)
+
+# # PCA
+# pcaing = PCA(n_components=2)
+# train_pca = pcaing.fit_transform(train)
+# test_pca = pcaing.transform(test)
+
+submission_file = pd.DataFrame.from_csv("sample_submission.csv")
+submission_file[list(submission_file.columns.values)] = predicted_results
+submission_file.to_csv("dep_fln002_rf.csv")
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import numpy as np
-import random
-from sklearn.decomposition import PCA
-from sklearn.linear_model import SGDClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import log_loss
-from sklearn.preprocessing import StandardScaler
-from sklearn.cross_validation import KFold
 
 __author__ = 'WiBeer'
 
+"""
+preprocessing data
+"""
 # preprocess test data
+print 'read train data'
 trainset = pd.DataFrame.from_csv('train.csv', index_col=1)
 trainset = trainset.fillna('-999')
 trainset[['Upc', 'FinelineNumber']] = trainset[['Upc', 'FinelineNumber']].astype(str)
@@ -23,6 +20,7 @@ n_trips = train_result.shape[0]
 train_data_not_count = pd.get_dummies(trainset['Weekday'])
 train_data_not_count = train_data_not_count.groupby(by=train_data_not_count.index, sort=False).mean()
 
+print 'dummy train DepartmentDescription'
 train_data_count_dep = pd.get_dummies(trainset['DepartmentDescription'])
 tmp_index = train_data_count_dep.index
 tmp_columns = list(train_data_count_dep.columns.values)
@@ -37,6 +35,7 @@ train_data_count_dep = train_data_count_dep.groupby(by=train_data_count_dep.inde
 train_data_tot_items = trainset['ScanCount'].groupby(by=trainset.index, sort=False).sum()
 
 # find most bought FinelineNumber
+print 'remove sparse train FinelineNumber'
 fineline_density = trainset['FinelineNumber'].value_counts()
 sparsity = n_trips * 0.02
 n_features = np.sum(fineline_density > sparsity)
@@ -55,6 +54,7 @@ trainset['FinelineNumber'] = tmp_series
 print trainset['FinelineNumber'].value_counts()
 
 # dummy fln
+print 'dummy train FinelineNumber'
 train_data_count_fln = pd.get_dummies(trainset['FinelineNumber'])
 tmp_index = train_data_count_fln.index
 tmp_columns = list(train_data_count_fln.columns.values)
@@ -67,7 +67,10 @@ train_data_count_fln = train_data_count_fln.groupby(by=train_data_count_fln.inde
 train = pd.concat([train_data_not_count, train_data_count_dep, train_data_count_fln, train_data_tot_items], axis=1)
 
 # preprocess test data
+print 'read test data'
 testset = pd.DataFrame.from_csv('test.csv', index_col=0)
+testset = testset.fillna('-999')
+
 test_data_not_count = pd.get_dummies(testset['Weekday'])
 test_data_not_count = test_data_not_count.groupby(by=test_data_not_count.index, sort=False).mean()
 n_test = testset.shape[0]
@@ -105,7 +108,7 @@ print testset['FinelineNumber'].value_counts()
 test_data_count_fln = pd.get_dummies(testset['FinelineNumber'])
 tmp_index = test_data_count_fln.index
 tmp_columns = list(test_data_count_fln.columns.values)
-tmp_table = np.array(test_data_count_fln) * np.array(testset['ScanCount']).reshape((n, 1))
+tmp_table = np.array(test_data_count_fln) * np.array(testset['ScanCount']).reshape((n_test, 1))
 test_data_count_fln = pd.DataFrame(tmp_table)
 test_data_count_fln.columns = tmp_columns
 test_data_count_fln.index = tmp_index
@@ -115,7 +118,9 @@ test = pd.concat([test_data_not_count, test_data_count_dep, test_data_count_fln,
 
 # Find common coloumns
 col_train = list(train.columns.values)
+print col_train
 col_test = list(test.columns.values)
+print col_test
 col_common = []
 # add only common columns for train and test
 for col in col_train:
@@ -123,49 +128,10 @@ for col in col_train:
         col_common.append(col)
 train = train[col_common]
 test = test[col_common]
-
-# Common preprocessing
-# Standardizing
-stding = StandardScaler()
-train = stding.fit_transform(train)
-test = stding.transform(test)
-
 print col_common
 
-classifier = RandomForestClassifier(n_estimators=200)
+print 'write to data'
+train.to_csv("train_dummied.csv")
+test.to_csv("test_dummied.csv")
+train_result.to_csv("train_result.csv")
 
-# # CV n = 4
-# cv_n = 4
-
-# metric = []
-# for train_index, test_index in kf:
-#     X_train, X_test = train_cv[train_index, :], train_cv[test_index, :]
-#     y_train, y_test = train_result_cv[train_index].ravel(), train_result_cv[test_index].ravel()
-#     print X_train.shape, X_test.shape
-#     print len(y_train), len(y_test )
-#     # train machine learning
-#     classifier.fit(X_train, y_train)
-
-#     # predict
-#     class_pred = classifier.predict_proba(X_test)
-
-#     # evaluate
-#     print log_loss(y_test, class_pred)
-#     metric.append(log_loss(y_test, class_pred))
-# print 'The log loss is: ', np.mean(metric)
-
-# predict testset
-classifier.fit(train, train_result)
-predicted_results = classifier.predict_proba(test)
-
-predicted_results_self = classifier.predict_proba(train)
-log_loss(train_result, predicted_results_self)
-
-# # PCA
-# pcaing = PCA(n_components=2)
-# train_pca = pcaing.fit_transform(train)
-# test_pca = pcaing.transform(test)
-
-submission_file = pd.DataFrame.from_csv("sample_submission.csv")
-submission_file[list(submission_file.columns.values)] = predicted_results
-submission_file.to_csv("trysub_rf.csv")
