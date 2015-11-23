6be2fbbab37330b153c07a236fcf1ae026a3d5ac
Yair Beer <yaia1309@gmail.com>
2015-11-23 08:05:57 +0200

    ensemble training

--- SDG_metaestimator.py
+++ SDG_metaestimator.py
@@ -17,27 +17,27 @@ train_result = np.array(train_result).ravel()
 
 train = pd.DataFrame.from_csv("train_dummied_150_sep_dep_fln_b_r_v5.csv").astype('float')
 train.fillna(0)
-# train_arr = np.array(train)
+train_arr = np.array(train)
 col_list = list(train.columns.values)
 
-# # print train_result.shape[1], ' categorial'
-# print train.shape[1], ' columns'
-#
-# print 'absing'
-# for i in range(train_arr.shape[0]):
-#     for j in range(train_arr.shape[1]):
-#         train_arr[i, j] = np.abs(train_arr[i, j])
-#
-# chi2_params = chi2(train_arr, train_result)
+# print train_result.shape[1], ' categorial'
+print train.shape[1], ' columns'
 
-# for i in range(train_arr.shape[1]):
-#     print col_list[i], chi2_params[0][i]
+print 'absing'
+for i in range(train_arr.shape[0]):
+    for j in range(train_arr.shape[1]):
+        train_arr[i, j] = np.abs(train_arr[i, j])
 
-# del train_arr
+chi2_params = chi2(train_arr, train_result)
+
+for i in range(train_arr.shape[1]):
+    print col_list[i], chi2_params[0][i]
+
+del train_arr
 
 best_metric = 10
 best_params = []
-param_grid = {'loss': ['log', 'modified_huber'], 'alpha': [0.03], 'n_iter': [200], 'chi2_lim': [1000],
+param_grid = {'loss': ['log', 'modified_huber'], 'alpha': [0.03], 'n_iter': [200], 'chi2_lim': [10000],
               'penalty': ['elasticnet'], 'l1_ratio': [0.15], 'n_jobs': [1]}
 # param_grid = {'loss': ['log'], 'alpha': [0.01], 'n_iter': [200],
 #               # 'chi2_lim': [1000],
@@ -46,14 +46,14 @@ param_grid = {'loss': ['log', 'modified_huber'], 'alpha': [0.03], 'n_iter': [200
 for params in ParameterGrid(param_grid):
     print params
 
-    # # filtering low chi2 cols
-    # chi2_lim = params['chi2_lim']
-    # chi2_cols = []
-    # for i in range(train.shape[1]):
-    #     if chi2_params[0][i] > chi2_lim:
-    #         chi2_cols.append(col_list[i])
+    # filtering low chi2 cols
+    chi2_lim = params['chi2_lim']
+    chi2_cols = []
+    for i in range(train.shape[1]):
+        if chi2_params[0][i] > chi2_lim:
+            chi2_cols.append(col_list[i])
 
-    # print len(chi2_cols), ' chi2 columns'
+    print len(chi2_cols), ' chi2 columns'
     train_arr = train.copy(deep=True)
     # train_arr = train_arr[chi2_cols]
 
@@ -94,4 +94,23 @@ for params in ParameterGrid(param_grid):
     meta_estimator = pd.DataFrame(meta_estimator)
     meta_estimator.index = train.index
     meta_estimator.to_csv('meta_SDG_' + params['loss'] + '.csv')
+
+    test = pd.DataFrame.from_csv("test_dummied_200_sep_dep_fln_b_r_v2.csv")
+    test.fillna(0)
+    test = test[chi2_cols]
+    test = np.array(test)
+    test = stding.transform(test)
+
+    predicted_results = classifier.predict_proba(test)
+
+    print 'writing to file'
+    submission_file = pd.DataFrame.from_csv("sample_submission.csv")
+    submission_file[list(submission_file.columns.values)] = predicted_results
+    submission_file.to_csv('meta_test_SDG_' + params['loss'] + '.csv')
+    # predict
+    class_pred = classifier.predict_proba(test)
+
+    # evaluate
+    # print log_loss(y_test, class_pred)
+    print log_loss(train_result, classifier.predict_proba(train))
 
