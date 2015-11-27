68ffefacac2b23caf5bea488bb99eb903d69a3af
Yair Beer <yaia1309@gmail.com>
2015-11-27 18:05:54 +0200

    ensemble training

--- lasagne_try.py
+++ lasagne_try.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Usage example employing Lasagne for digit recognition using the MNIST dataset.
+Usage example employing Lasagne.
 
 This example is deliberately structured as a long flat file, focusing on how
 to use Lasagne, instead of focusing on writing maximally modular and reusable
@@ -15,63 +15,26 @@ a separate repository: https://github.com/Lasagne/Recipes
 from __future__ import print_function
 
 import sys
-import os
 import time
 
 import numpy as np
+import pandas as pd
 import theano
 import theano.tensor as T
 
 import lasagne
 
+# ################## Prepare the dataset ##################
 
-# ################## Download and prepare the MNIST dataset ##################
-# This is just some way of getting the MNIST dataset from an online location
-# and loading it into numpy arrays. It doesn't involve Lasagne at all.
 
 def load_dataset():
-    # We first define a download function, supporting both Python 2 and 3.
-    if sys.version_info[0] == 2:
-        from urllib import urlretrieve
-    else:
-        from urllib.request import urlretrieve
-
-    def download(filename, source='http://yann.lecun.com/exdb/mnist/'):
-        print("Downloading %s" % filename)
-        urlretrieve(source + filename, filename)
-
-    # We then define functions for loading MNIST images and labels.
-    # For convenience, they also download the requested files if needed.
-    import gzip
-
-    def load_mnist_images(filename):
-        if not os.path.exists(filename):
-            download(filename)
-        # Read the inputs in Yann LeCun's binary format.
-        with gzip.open(filename, 'rb') as f:
-            data = np.frombuffer(f.read(), np.uint8, offset=16)
-        # The inputs are vectors now, we reshape them to monochrome 2D images,
-        # following the shape convention: (examples, channels, rows, columns)
-        data = data.reshape(-1, 1, 28, 28)
-        # The inputs come as bytes, we convert them to float32 in range [0,1].
-        # (Actually to range [0, 255/256], for compatibility to the version
-        # provided at http://deeplearning.net/data/mnist/mnist.pkl.gz.)
-        return data / np.float32(256)
-
-    def load_mnist_labels(filename):
-        if not os.path.exists(filename):
-            download(filename)
-        # Read the labels in Yann LeCun's binary format.
-        with gzip.open(filename, 'rb') as f:
-            data = np.frombuffer(f.read(), np.uint8, offset=8)
-        # The labels are vectors of integers now, that's exactly what we want.
-        return data
 
     # We can now download and read the training and test set images and labels.
-    X_train = load_mnist_images('train-images-idx3-ubyte.gz')
-    y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
-    X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
-    y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
+    X_train = np.array(pd.DataFrame.from_csv('train_dummied_150_sep_dep_fln_b_r_v5.csv')).astype('float')
+    # print (X_train.shape)
+    y_train = np.array(pd.DataFrame.from_csv("train_result.csv")).ravel()
+    X_test = np.array(pd.DataFrame.from_csv('test_dummied_150_sep_dep_fln_b_r_v5.csv')).astype('float')
+    y_test = np.ones((X_test.shape[0],)) * 3.0
 
     # We reserve the last 10000 training examples for validation.
     X_train, X_val = X_train[:-10000], X_train[-10000:]
@@ -95,7 +58,7 @@ def build_mlp(input_var=None):
     # Input layer, specifying the expected input shape of the network
     # (unspecified batchsize, 1 channel, 28 rows and 28 columns) and
     # linking it to the given Theano variable `input_var`, if any:
-    l_in = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
+    l_in = lasagne.layers.InputLayer(shape=(None, 1, 1571),
                                      input_var=input_var)
 
     # Apply 20% dropout to the input data:
@@ -140,7 +103,7 @@ def build_custom_mlp(input_var=None, depth=2, width=800, drop_input=.2,
     # just used different names above for clarity.
 
     # Input layer and dropout (with shortcut `dropout` for `DropoutLayer`):
-    network = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
+    network = lasagne.layers.InputLayer(shape=(None, 1, 1571),
                                         input_var=input_var)
     if drop_input:
         network = lasagne.layers.dropout(network, p=drop_input)
@@ -162,7 +125,7 @@ def build_cnn(input_var=None):
     # and a fully-connected hidden layer in front of the output layer.
 
     # Input layer, as usual:
-    network = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
+    network = lasagne.layers.InputLayer(shape=(None, 1, 1571),
                                         input_var=input_var)
     # This time we do not apply input dropout, as it tends to work less well
     # for convolutional layers.
@@ -234,7 +197,7 @@ def main(model='mlp', num_epochs=500):
     X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
 
     # Prepare Theano variables for inputs and targets
-    input_var = T.tensor4('inputs')
+    input_var = T.tensor3('inputs')
     target_var = T.ivector('targets')
 
     # Create neural network model (depending on first command line parameter)
